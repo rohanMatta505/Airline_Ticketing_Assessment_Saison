13cbccda44319f76ded653a662db84d54f92d714
Rohan Matta <rohanmatta@Rohans-Laptop.local>
2024-11-11 21:12:28 +0530

    updated properties file and pom file for integrating dummy H2 database

--- src/main/java/com/example/airline_ticketing_assessment_saison/repository/BookingRepository.java
+++ src/main/java/com/example/airline_ticketing_assessment_saison/repository/BookingRepository.java
@@ -1,10 +1,7 @@
 package com.example.airline_ticketing_assessment_saison.repository;
 
 import com.example.airline_ticketing_assessment_saison.entity.Booking;
-import com.example.airline_ticketing_assessment_saison.entity.Flight;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    Booking findByFlightAndSeatSelected(Long flightId, int seatSelected);
     Booking findById(Long bookingId);
-
 }
